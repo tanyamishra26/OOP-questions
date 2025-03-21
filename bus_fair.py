8b27a109a51451e999df290b87c6e481e38ba770
tanyamishra26 <tanyamishra260@gmail.com>
2025-03-21 23:10:22 +0530

    new question

---
+++ bus_fair.py
@@ -0,0 +1,14 @@
+class Vehicle:
+    def __init__(self,seating_capacity):
+        self.seating_capacity=seating_capacity
+    def fare(self):
+        return self.seating_capacity*100
+
+class Bus(Vehicle):
+    def fare(self):
+        base_fare=super().fare()
+        total_fare=base_fare+(0.1*base_fare)
+        return total_fare
+
+bus=Bus(50)
+print("The Total fare for the bus is:",bus.fare())
