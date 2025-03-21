3562ef4dee5bfcef9fe1bf65686d46f211ce6c00
tanyamishra26 <tanyamishra260@gmail.com>
2025-03-21 20:43:10 +0530

    incomplete question

---
+++ scoop_nd_bowl.py
@@ -0,0 +1,25 @@
+# class Scoop:
+#     def __init__(self,flavor):
+#         self.flavor=flavor
+#         self.__price=None
+    
+#     def get_price(self):
+#         return self.__price
+#     def set_price(self,price):
+#         self.__price=price
+    
+# class Bowl:
+#     def __init(self):
+#         self.__scoop_list=[]
+    
+#     def add_scoop(self,*agrs):
+#         for scoop in args:
+#             self.__scoop_list.append(scoop)
+#     def display(self):
+#         "Displaying Bowl"
+#         for scoop in self.__scoop_list:
+#             print(f"Flavor - {scoop} Price - {get_price()}")
+
+
+
+
